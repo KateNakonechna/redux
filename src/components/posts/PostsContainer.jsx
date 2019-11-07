de3030342a1521cad59e003f42c669af6d273f81
Kateryna Nakonechna <k.nakonechna@levi9.com>
2019-11-07 17:22:18 +0200

    add useDispatch

--- src/components/posts/PostsContainer.jsx
+++ src/components/posts/PostsContainer.jsx
@@ -3,7 +3,6 @@ import { connect } from "react-redux";
 import { fetchPosts } from "../../store/posts/actions";
 import PostList from "./PostList";
 import { useSelector, useDispatch } from "react-redux";
-import { GET_POSTS } from "../../store/posts/actions";
 
 const PostsContainer = () => {
   const posts = useSelector(state => state.posts);
@@ -13,14 +12,7 @@ const PostsContainer = () => {
   // }
 
   useEffect(() => {
-    fetch("https://jsonplaceholder.typicode.com/posts")
-      .then(res => res.json())
-      .then(posts =>
-        dispatch({
-          type: GET_POSTS,
-          payload: posts
-        })
-      );
+    dispatch(fetchPosts());
     console.log(posts);
     // return () => {
     //   cleanup
@@ -47,5 +39,10 @@ const PostsContainer = () => {
 // const mapStateToProps = state => ({
 //   posts: state.posts.posts
 // });
+
+// export default connect(
+//   mapStateToProps,
+//   mapDispatchToProps
+// )(PostsContainer);
 
 export default PostsContainer;
--- src/store/posts/actions.js
+++ src/store/posts/actions.js
@@ -1,13 +1,12 @@
+export const GET_POSTS = "GET_POSTS";
 
-export const GET_POSTS = 'GET_POSTS';
+export const getPostAction = posts => ({
+  type: GET_POSTS,
+  payload: posts
+});
 
 export const fetchPosts = () => dispatch => {
-    fetch('https://jsonplaceholder.typicode.com/posts')
-      .then(res => res.json())
-      .then(posts =>
-        dispatch({
-          type: GET_POSTS,
-          payload: posts
-        })
-      );
-  };
+  fetch("https://jsonplaceholder.typicode.com/posts")
+    .then(res => res.json())
+    .then(posts => dispatch(getPostAction(posts)));
+};
